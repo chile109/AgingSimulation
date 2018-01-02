a3c546c8c355c179f684deda2332b4db1015bdef
chile109 <chile109@gmail.com>
2018-01-03 01:48:22 +0800

    [Add] 新增Tag Weapon [Add] Boss 狀態機  武器互動邏輯

---
+++ Assets/Script/BossController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+
+    public int Hp;
+    BossState NowState;
+
+    private IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start()
+    {
+        Hp = 100;
+        NowState = BossState.Idle;
+
+        coroutine = BeAngry(5f);
+        StartCoroutine(coroutine);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Weapon" && NowState == BossState.Angry)
+        {
+            Hp -= 10;
+            Debug.Log("HP:" + Hp);
+            NowState = BossState.Injured;
+
+            Debug.Log("Boss Injured");
+            StopAllCoroutines();
+            coroutine = BeIdle(3f);
+            StartCoroutine(coroutine); ;
+        }
+    }
+
+    private IEnumerator BeAngry(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        NowState = BossState.Angry;
+        Debug.Log("Boss Angry");
+
+        coroutine = Attack(5f);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Attack(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        NowState = BossState.Attack;
+        Debug.Log("Boss Attack");
+
+        coroutine = BeIdle(3f);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator BeIdle(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        NowState = BossState.Idle;
+        Debug.Log("Boss Idle");
+
+        coroutine = BeAngry(5f);
+        StartCoroutine(coroutine);
+    }
+}
+
+    public enum BossState
+{
+    Idle,
+    Angry,
+    Attack,
+    Injured,
+    Dead,
+}
