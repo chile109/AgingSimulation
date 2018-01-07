e149735ca6fdf2d6fca84ff8ba5be2b7444a7134
chile109 <chile109@gmail.com>
2018-01-07 16:07:12 +0800

    [Add] 貓頭鷹對話系統

---
+++ Assets/Script/DiologPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiologPlayer : MonoBehaviour {
+
+    public Text Owlsaying;
+    public SteamVR_TrackedObject trackedObj;
+    public string[] Dialog;
+    private int id;
+
+    void Start () {
+        id = 0;
+        Owlsaying.text = Dialog[id];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (id < Dialog.Length - 1)
+            {
+                id += 1;
+                Owlsaying.text = Dialog[id];
+            }
+
+            else
+                Owlsaying.text = "Start";
+        }
+    }
+}
